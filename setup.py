2545aa92bfa7d24e084b566fa22e93edbbfb2cfa
Sotunde Abiodun <sotundeabiodun00@gmail.com>
2022-10-15 23:01:21 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/IamAbbey/py_sync_dotenv",
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
